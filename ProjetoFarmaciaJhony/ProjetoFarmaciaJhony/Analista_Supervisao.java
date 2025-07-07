dcf2366650aa7a8038f4ca4a35217d90913c689b
Jhonath Miranda de Sousa <jjhonathmmiranda@gmail.com>
2025-07-07 17:52:39 -0300

    Adicionando classe: Analista_Supervisão

---
+++ ProjetoFarmaciaJhony/ProjetoFarmaciaJhony/Analista_Supervisao.java
@@ -0,0 +1,12 @@
+package classes_analista_supervisao;
+
+public class Analista_Supervisao{
+    int id_analista;
+    
+    void Emitirrelatoriofinal () {}
+    void Emitirfeedback () {}
+    
+    public Analista_Supervisao (int id_analista) {
+        this.id_analista = id_analista;
+    }
+}
