3a9dc0a854e243627e677e2a375843753d171d11
Michelly1 <127505165+Michelly1@users.noreply.github.com>
2025-07-07 08:31:02 -0300

    commit de kaio

---
+++ GerentedeSupervisao.java
@@ -0,0 +1,54 @@
+package classesLT;
+
+public class GerentedeSupervisao {
+    
+    int id_gerentesup;
+
+    public GerentedeSupervisao(int id_gerentesup) {
+        this.id_gerentesup = id_gerentesup;
+    }
+    
+    
+    
+    
+    void checarestoque(){
+    }
+    
+    void cadastrarfornecedor(){
+    }
+    
+    void emitirfeedback(){
+    }
+    
+    void emitirrelatoriofinan(){
+    }
+    
+    void telalupa(){
+    }
+    
+    void telaprincip(){
+    }
+    
+    void editarinfocadastro(){
+    }
+    
+    void cadastrarconvenio(){
+    }
+    
+    void cadastrarfuncionario(){
+    }
+    
+    void cadastrarVenda() {
+    }
+
+    void cadastrarFeedback() {
+    }
+
+    void cadastrarReembolso() {
+    }
+
+    void cadastrarrem_contro() {
+    }
+}
+    
+
