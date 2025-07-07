7b5f7a07380339f54f724860ec2ad44bb0621dfa
juliaerica <20221IREINFINT0007@ifba.edu.br>
2025-07-07 17:08:00 -0300

    Adicionando classes: Funcionário e Gerente

---
+++ farmaciajulia/farmaciajulia/Funcionario.java
@@ -0,0 +1,28 @@
+
+package farmaciajulia;
+
+public class Funcionario {
+    
+    String nome;
+    double cpf;
+    String categoria;
+    String endereco;
+    double telefone;
+
+    public Funcionario(String nome, double cpf, String categoria, String endereco, double telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.categoria = categoria;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+    
+    
+    void Telaprincipal (){
+        
+    }
+    
+    void Telalupa (){
+        
+    }
+}
---
+++ farmaciajulia/farmaciajulia/Gerente.java
@@ -0,0 +1,34 @@
+
+package farmaciajulia;
+
+public class Gerente {
+    
+    int idGerente;
+
+    public Gerente() {
+        this.idGerente = idGerente;
+    }
+     
+    
+    void cadastrarFuncionario (){ 
+    }
+    
+    void cadastraConvenio (){   
+    }
+    
+    void editarCadastro (){    
+    }
+    
+    void emitirRelatorioFinan (){     
+    }
+    
+    void EmitirFeedback (){    
+    }
+    
+    void cadastrarFornecedor (){
+    }
+    
+    void checarEstoque (){
+    }
+    
+}
