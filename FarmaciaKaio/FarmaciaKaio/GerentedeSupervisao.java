2cbb790732c08c9135ac4177f63469cbe6648dda
Kaiozs13 <20221ireinfint0013@ifba.edu.br>
2025-07-07 16:37:52 -0300

    Adicionando classe: gerente de supervisão

---
+++ FarmaciaKaio/FarmaciaKaio/GerentedeSupervisao.java
@@ -0,0 +1,50 @@
+package classesLT;
+
+public class GerentedeSupervisao {
+    
+    int id_gerentesup;
+    
+    
+    
+    
+    void checarestoque(){
+    }
+    
+    void cadastrarfornecedor(){
+    }
+    
+    void emitirfeedback(){
+    }
+    
+    void emitirrelatoriofinan(){
+    }
+    
+    void telalupa(){
+    }
+    
+    void telaprincip(){
+    }
+    
+    void editarinfocadastro(){
+    }
+    
+    void cadastrarconvenio(){
+    }
+    
+    void cadastrarfuncionario(){
+    }
+    
+    void cadastrarVenda() {
+    }
+
+    void cadastrarFeedback() {
+    }
+
+    void cadastrarReembolso() {
+    }
+
+    void cadastrarrem_contro() {
+    }
+}
+    
+
